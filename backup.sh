771c457539cf89b752b088035197e54c538228e4
THEKINGAF <THEKINGAF@users.noreply.github.com>
2021-11-28 15:37:23 +0100

    added temp folder

--- backup.sh
+++ backup.sh
@@ -12,6 +12,9 @@ done
 # Stop script on error
 set -o pipefail -e
 
+# Create temp folder
+tmp_dir=$(mktemp -d -t backup-XXXXXXXXXX)
+
 # Parse config file and set variables
 if [[ ! -f "$config_file" ]]; then
     >&2 echo "ERROR: config file not found"
@@ -23,7 +26,8 @@ index_file=$( jq -r .index_file "$config_file" )
 if [[ ! -f "$index_file" ]]; then
     full_backup=true
 fi
-output_file="$( jq -r .backup_prefix "$config_file" )_$date"
+output_file=$tmp_dir"/"
+output_file+="$( jq -r .backup_prefix "$config_file" )_$date"
 if  [ "$full_backup" = true ]; then
     output_file+="_F" # set flag "F" for Full backup
 else
@@ -39,15 +43,21 @@ if  [ "$full_backup" = true ]; then
 fi
 
 # Create archive, create/update index file and encrypt
-tar --create --gzip --listed-incremental="$index_file" --verbose "${source_folders[@]}" | gpg --output "$output_file" --encrypt --recipient "$pgp_recipient"
+tar --create --gzip --listed-incremental="$index_file" "${source_folders[@]}" | gpg --output "$output_file" --encrypt --recipient "$pgp_recipient"
+
+echo "archiving and encryption finished"
 
 # Upload
 gsutil cp "$output_file" "gs://$bucket/"
+
+echo "upload finished"
 
 # Remove local backup file
 rm "$output_file"
 
 # Unset "stop script on error"
 set +o pipefail +e
+
+rm -rf $tmp_dir
 
 exit 0
